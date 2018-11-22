dfea6a54297f359f87ebbe2a42407b6164f827e3
sylvie <ngh.sylvie@gmail.com>
2018-11-22 10:03:46 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App-logo">`MEOW` ^(=*w*=)^</div>;
+  }
+}
+
+export default App;
