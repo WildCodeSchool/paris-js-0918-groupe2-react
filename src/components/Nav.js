2fa1412b070c8bc2bcc7d7ca86ccbd8d28c036f2
Lucie Duplat <lucie.duplat@gmail.com>
2018-11-23 10:15:58 +0100

    Dashboard

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
+import icone_mon_compte from "./Icones_Arigoni/icone_mon_compte.png";
+import icone_creanciers from "./Icones_Arigoni/icone_creanciers.png";
+import icone_debiteurs from "./Icones_Arigoni/icone_debiteurs.png";
+import icone_centre_actions from "./Icones_Arigoni/icone_centre_actions.png";
+import icone_historique from "./Icones_Arigoni/icone_historique.png";
+import "./Nav.css";
 
 class Nav extends React.Component {
   render() {
     return (
-      <div>
-        <div className="fl w-25 ba-ns vh-100 ">
-          <h1 className="tc bb mt3 h3">TABLEAU DE BORD</h1>
-          <nav className="">
-            <a className="f3 b db tc mt6 no-underline hover-red" href="">
+      <div className="fl w-25 ba-ns b--transparent allNav white">
+        <div className="topNav">
+          <h1 className="ml5 mt4 bb b--transparent h3">TABLEAU DE BORD</h1>
+        </div>
+        <div className="colorNav">
+          <nav className="ml4 navPage">
+            <a
+              className="f3 b db mb6 mt5 no-underline hover-white grow"
+              href=""
+            >
+              <img src={icone_mon_compte} alt="compte" className="iconeNav" />{" "}
               Mon compte
             </a>
-            <a className="f3 b db tc mt6 no-underline hover-red" href="">
+            <a className="f3 b db mb6 no-underline hover-white grow" href="">
+              <img src={icone_creanciers} alt="compte" className="iconeNav" />{" "}
               Créanciers
             </a>
-            <a className="f3 b db tc mt6 no-underline hover-red" href="">
+            <a className="f3 b db mb6 no-underline hover-white grow" href="">
+              <img src={icone_debiteurs} alt="compte" className="iconeNav" />{" "}
               Débiteurs
             </a>
-            <a className="f3 b db tc mt6 no-underline hover-red" href="">
+            <a className="f3 b db mb6 no-underline hover-white grow" href="">
+              <img
+                src={icone_centre_actions}
+                alt="compte"
+                className="iconeNav"
+              />{" "}
               Actions
             </a>
-            <a className="f3 b db tc mt6 no-underline hover-red" href="">
+            <a className="f3 b db no-underline hover-white grow" href="">
+              <img src={icone_historique} alt="compte" className="iconeNav" />{" "}
               Historique
             </a>
           </nav>
