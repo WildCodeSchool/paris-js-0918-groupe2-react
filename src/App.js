0ff2e3e099fac01182c16dfeba359bca19a644f4
Mérédith Mougeot <meredith75017@hotmail.fr>
2018-11-23 11:27:08 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import Creanciers from "./components/creanciers";
 import Debiteurs from "./components/debiteurs";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Creanciers />
-        <Debiteurs />
+        <Header />
       </div>
     );
   }
