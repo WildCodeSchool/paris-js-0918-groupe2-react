f53718feed3deaa605dd7c0f48f17ea05f8b3d9b
Electric_Sheep <iamthebou@gmail.com>
2018-11-23 10:34:51 +0100

    resolved conflicts during merge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
-<<<<<<< HEAD
 import Creanciers from "./components/creanciers";
 import Debiteurs from "./components/debiteurs";
 import Formulairecreancier from "./components/formulairecreancier";
-=======
 import Nav from "./components/Nav";
->>>>>>> origin/navbar
 
 class App extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <Creanciers />
-        <Debiteurs />
-        <Formulairecreancier />
-=======
+        {/* <Creanciers /> */}
+        {/* <Debiteurs /> */}
+        {/* <Formulairecreancier /> */}
         <Nav />
->>>>>>> origin/navbar
       </div>
     );
   }
