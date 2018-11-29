a5595f4e87df666aaf0ec8dff40537474747c485
Electric_Sheep <iamthebou@gmail.com>
2018-11-29 09:39:14 +0100

    created the actions component

---
+++ src/components/actions.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Actions extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p className="fl w-100">pouet</p>
+      </div>
+    );
+  }
+}
+
+export default Actions;
