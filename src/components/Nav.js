718e7d6d28e23d20a468cdd1d530150786a4efa3
sylvie <ngh.sylvie@gmail.com>
2018-11-29 11:48:33 +0100

    commit dev2

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,13 +5,11 @@ import icone_debiteurs from "./Icones_Arigoni/icone_debiteurs.png";
 import icone_centre_actions from "./Icones_Arigoni/icone_centre_actions.png";
 import icone_historique from "./Icones_Arigoni/icone_historique.png";
 import "./Nav.css";
-import { NavLink } from "react-router-dom";
 
 class Nav extends Component {
   render() {
     return (
       <div className=" ba-ns b--transparent allNav white">
-      
         <div className="topNav">
           <h1 className="ml4 mt4 bb b--transparent h3">TABLEAU DE BORD</h1>
         </div>
@@ -24,24 +22,15 @@ class Nav extends Component {
               <img src={icone_mon_compte} alt="compte" className="iconeNav" />{" "}
               Mon compte
             </a>
-            <a
-              className="f3 b db mb6 no-underline hover-white grow"
-              href="#"
-            >
+            <a className="f3 b db mb6 no-underline hover-white grow" href="#">
               <img src={icone_creanciers} alt="compte" className="iconeNav" />{" "}
               Créanciers
             </a>
-            <a
-              className="f3 b db mb6 no-underline hover-white grow"
-              href="#"
-            >
+            <a className="f3 b db mb6 no-underline hover-white grow" href="#">
               <img src={icone_debiteurs} alt="compte" className="iconeNav" />{" "}
               Débiteurs
             </a>
-            <a
-              className="f3 b db mb6 no-underline hover-white grow"
-              href="#"
-            >
+            <a className="f3 b db mb6 no-underline hover-white grow" href="#">
               <img
                 src={icone_centre_actions}
                 alt="compte"
@@ -49,10 +38,7 @@ class Nav extends Component {
               />{" "}
               Actions
             </a>
-            <a
-              className="f3 b db no-underline hover-white grow"
-              href="#"
-            >
+            <a className="f3 b db no-underline hover-white grow" href="#">
               <img src={icone_historique} alt="compte" className="iconeNav" />{" "}
               Historique
             </a>
