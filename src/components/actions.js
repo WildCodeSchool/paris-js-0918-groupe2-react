7b7c611551fb21e4bf9f351900f76329d9363913
Audrey LOURI <audrey.louri@gmail.com>
2018-11-29 10:49:50 +0100

    navlink nav

--- src/components/actions.js
+++ src/components/actions.js
@@ -1,19 +1,14 @@
 import React, { Component } from "react";
-import Tabfacture from "./tabfacture";
-import Tabavoir from "./tabavoir";
+import Tabfacture from "./Tabfacture";
+import Tabavoir from "./Tabavoir";
 
 class Actions extends Component {
   state = {};
   render() {
     return (
-<<<<<<< HEAD
-      <div className="fl w-100">
-        <p> </p>
-=======
       <div className="fl w-100 pa4 ">
         <Tabfacture />
         <Tabavoir />
->>>>>>> e7463b2f425223637dc0fbf34ca995bd91cdca79
       </div>
     );
   }
