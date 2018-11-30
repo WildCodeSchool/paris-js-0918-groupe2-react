c63919bbf698b74af2f774e1dbf7b747dac859c9
Mérédith Mougeot <meredith75017@hotmail.fr>
2018-11-30 10:58:24 +0100

    tableau acomptes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
-import dashboard from "./components/dashboard";
-import Login from "./components/LoginPage";
-import creanciers from "./components/creanciers";
-import debiteurs from "./components/debiteurs";
-import actions from "./components/actions";
-import Historique from "./components/Historique";
+// import { Switch, Route } from "react-router-dom";
+// import dashboard from "./components/dashboard";
+// import Login from "./components/LoginPage";
+// import creanciers from "./components/creanciers";
+// import debiteurs from "./components/debiteurs";
+// import actions from "./components/actions";
+// import Historique from "./components/Historique";
+ import Acomptes from "./components/Acomptes";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Switch>
-          <Route exact path="/" component={Login} />
+     {/* <Switch>
+        <Route exact path="/" component={Login} />
           <Route exact path="/dashboard" component={dashboard} />
           <Route exact path="/creanciers" component={creanciers} />
           <Route exact path="/debiteurs" component={debiteurs} />
           <Route exact path="/actions" component={actions} />
           <Route exact path="/historique" component={Historique} />
-        </Switch>
+        </Switch>  */}
+        <Acomptes />
       </div>
     );
   }
