b5e2458664824ed06d4965e9bb75b2e09eeb1507
Audrey LOURI <audrey.louri@gmail.com>
2018-11-30 14:36:29 +0100

    add router page facture

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -83,6 +83,19 @@ class Nav extends Component {
                   Historique
                 </p>
               </NavLink>
+              <NavLink
+                to="/dashboard/actions/factures"
+                onClick={() => this.props.pageChange("factures")}
+              >
+                <p className="f3 b db no-underline hover-white grow">
+                  <img
+                    src={icone_historique}
+                    alt="compte"
+                    className="iconeNav"
+                  />{" "}
+                  Historique
+                </p>
+              </NavLink>
             </nav>
           </div>
         </div>
