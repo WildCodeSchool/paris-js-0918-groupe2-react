36472fe5a99838745d6c8694f02725ab204703ea
Audrey LOURI <audrey.louri@gmail.com>
2018-11-30 14:25:31 +0100

    add infos cabinet

--- src/components/compte.js
+++ src/components/compte.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import "./compte.css";
+import modifier from "./Icones_Arigoni/icone_modifier.png";
+import supprimer from "./Icones_Arigoni/icone_supprimer.png";
+import upload from "./Icones_Arigoni/icone_upload.png";
+import signature from "./Icones_Arigoni/signature.png";
 
 class Compte extends Component {
   state = {};
@@ -107,6 +111,50 @@ class Compte extends Component {
         </div>
         <div className="fl w-40 pt3 tc">
           <span className="f1 b">Cabinet Arigoni</span>
+          <div className="ba mt3 w-60-ns nested-copy-line-height tc b--gray firstBorder">
+            <p className="b black">
+              {" "}
+              Maître Arigoni Alexandra <br />
+              38 avenue Hoche 75008 Paris <br />
+              +33 (0)1 53 75 79 00 <br />
+              +33 (0)1 53 75 00 15 <br />
+              a.arigoni@arigoni-avocat.com <br />
+              TVA FR 34453740755
+            </p>
+          </div>
+          <div className="ba mt3 w-60-ns nested-copy-line-height tc pb2 h4 b--gray otherBorder">
+            <p className="b black tl ml3">
+              {" "}
+              En-tête:{" "}
+              <span className="athelas navy f4 ml4">Alexandra Arigoni </span>
+              <br /> <span className="athelas navy f4 job"> AVOCAT</span>
+            </p>
+
+            <img className="icone pointer ml3" src={upload} alt="upload" />
+            <img className="icone pointer ml2" src={modifier} alt="modifier" />
+            <img
+              className="icone pointer ml2"
+              src={supprimer}
+              alt="supprimer"
+            />
+          </div>
+          <div className="ba mt3 w-60-ns nested-copy-line-height tc pb2 h4 b--gray otherBorder">
+            <p className="b black tl ml3"> Signature: </p>
+            <img
+              className="w-33-ns ml3 h3 signature"
+              src={signature}
+              alt="signature"
+            />{" "} <br />
+            <div className="mt3">
+            <img className="icone pointer ml3" src={upload} alt="upload" />
+            <img className="icone pointer ml2" src={modifier} alt="modifier" />
+            <img
+              className="icone pointer ml2"
+              src={supprimer}
+              alt="supprimer"
+            />
+            </div>
+          </div>
         </div>
       </div>
     );
