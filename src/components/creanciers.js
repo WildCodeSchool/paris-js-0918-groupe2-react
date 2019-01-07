25c32748a3e0dae4ae2e9caf4d5ab1ebf9817421
sylvie <ngh.sylvie@gmail.com>
2019-01-07 16:26:06 +0100

    merge

--- src/components/creanciers.js
+++ src/components/creanciers.js
@@ -198,9 +198,9 @@ class Creanciers extends Component {
             {/* Button créer un créancier */}
             <div className="buttoncreancier tc pt4">
               <NavLink
-                to="/dashboard/formAvoir"
+                to="/dashboard/formCreancier"
                 className="f6 link dim br1 ph3 pv2 mt2 mb4 dib white bg-dark-blue "
-                onClick={() => this.props.pageChangeSub("Formulaireavoir")}
+                onClick={() => this.props.pageChangeSub("FormCreancier")}
               >
                 Créer un créancier
               </NavLink>
