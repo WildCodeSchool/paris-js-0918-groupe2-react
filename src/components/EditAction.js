ebde62468a7d2c2af0f4293eb32bac0a3d2528ac
Audrey LOURI <audrey.louri@gmail.com>
2019-01-22 10:53:25 +0100

    docxtemplater

--- src/components/EditAction.js
+++ src/components/EditAction.js
@@ -220,6 +220,18 @@ class EditAction extends Component {
     ).then(data => window.open(`http://localhost:4848/documents/${nom}`));
   };
 
+  handleInjonction = () => {
+    Axios.get(
+      `http://localhost:4848/api/documents/createInjonction/${this.props.actionId}`
+    );
+  };
+
+  handleRecap = () => {
+    Axios.get(
+      `http://localhost:4848/api/documents/createRecap/${this.props.actionId}`
+    )
+  };
+
   handleDelete = (id, denomination, type) => {
     const typeMessageInitial = whatType => {
       if (whatType === "factures") {
@@ -1095,6 +1107,7 @@ class EditAction extends Component {
                   <span
                     className="f6 link dim br1 ph3 pv2 mt4 mb4 mr6 dib white bg-dark-blue"
                     href="#0"
+                    onClick={() => this.handleInjonction()}
                   >
                     Générer injonction de payer
                   </span>
@@ -1105,6 +1118,7 @@ class EditAction extends Component {
                   <span
                     className="f6 link dim br1 ph3 pv2 mt4 mb4 mr6 dib white bg-dark-blue"
                     href="#0"
+                    onClick={() => this.handleRecap()}
                   >
                     Générer tableau récapitulatif
                   </span>
